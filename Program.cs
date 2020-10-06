86c5bb908e1e8ca6df8218047b7b509ddbad472e
Gnosolmu <ollicook@gmail.com>
2020-10-06 02:52:47 -0600

    initializing new dotnet console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bible_verse_counts_per_chapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
